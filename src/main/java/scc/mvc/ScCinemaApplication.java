bb9436fc2e910315346873767b51650e9e18b258
choijeongnam <cjn4336@gmail.com>
2021-07-16 10:53:46 +0900

    setting

---
+++ src/main/java/scc/mvc/ScCinemaApplication.java
@@ -0,0 +1,13 @@
+package scc.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/scc/mvc/ScCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package scc.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
