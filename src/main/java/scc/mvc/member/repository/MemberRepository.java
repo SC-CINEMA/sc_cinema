974cefbf183d46a0e799bd91d3c8c81b34d66b37
songjayhyun <thewinner1st@naver.com>
2021-07-18 16:09:38 +0900

    리포지토리

--- src/main/java/scc/mvc/member/repository/MemberRepository.java
+++ src/main/java/scc/mvc/member/repository/MemberRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import scc.mvc.member.domain.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
 }
