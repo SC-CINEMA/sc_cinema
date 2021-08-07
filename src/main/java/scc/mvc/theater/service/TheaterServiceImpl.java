a7040371ee00df943a1bce3ddf3636c8f7fe79cd
songjayhyun <thewinner1st@naver.com>
2021-08-07 13:35:30 +0900

    Update TheaterServiceImpl.java

--- src/main/java/scc/mvc/theater/service/TheaterServiceImpl.java
+++ src/main/java/scc/mvc/theater/service/TheaterServiceImpl.java
@@ -1,5 +1,25 @@
 package scc.mvc.theater.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import scc.mvc.theater.domain.Theater;
+import scc.mvc.theater.repository.TheaterRepository;
+
 public class TheaterServiceImpl implements TheaterService {
+
+	TheaterRepository theaterRepository;
+	
+	@Autowired
+	public TheaterServiceImpl(TheaterRepository theaterRepository) {
+		super();
+		this.theaterRepository = theaterRepository;
+	}
+
+	@Override
+	public void enroll(Theater theater) {
+
+		theaterRepository.save(theater);
+		
+	}
 
 }
